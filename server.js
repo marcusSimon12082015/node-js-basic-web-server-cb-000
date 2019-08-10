f7c6ef22b2cc8646c45cebd0e48d4bd39099a41f
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-08-10 18:50:13 +0000

    Automatically backed up by Learn

--- server.js
+++ server.js
@@ -3,12 +3,53 @@
 const http         = require('http');
 const finalhandler = require('finalhandler');
 const Router       = require('router');
+const bodyParser = require('body-parser');
 
-const router = new Router();
+const router = new Router({ mergeParams: true });
+
+let messages = [];
+let nextId = 1;
+
+class Message {
+  constructor(message) {
+    this.id = nextId;
+    this.message = message;
+    nextId++;
+  }
+}
+
+router.use(bodyParser.json());
 
 router.get('/', (request, response) => {
-  // A good place to start!
-  response.end();
+  response.setHeader('Content-Type', 'text/plain; charset=utf-8')
+  response.end('Hello, World!');
+});
+
+router.post('/message',(request,response) =>{
+  response.setHeader('Content-Type', 'application/json; charset=utf-8');
+  let newMsg = new Message(request.body.message);
+  messages.push(newMsg);
+  response.end(JSON.stringify(newMsg.id));
+});
+
+router.get('/messages',(request,response) => {
+  response.setHeader('Content-Type', 'application/json; charset=utf-8');
+  response.end(JSON.stringify(messages));
+});
+
+var handler = Router({ mergeParams: true });
+router.use('/message/:id', handler);
+
+handler.get('/',(request,response) => {
+  response.setHeader('Content-Type', 'application/json; charset=utf-8');
+  let message = messages.find(message => message.id === id);
+  if (message === 'undefined') {
+    response.status = 404;
+    response.statusMessage = "No message found";
+    response.end();
+    return;
+  }
+  response.end(JSON.stringify(message));
 });
 
 const server = http.createServer((request, response) => {
